0c26eccd03cb37f0a28767dc02d8de4f52c41c79
문성하 <azqazq195@gmail.com>
2024-12-22 19:14:02 +0900

    init

---
+++ spring/annotationprocessor/src/main/java/com/moseoh/annotationprocessor/AnnotationprocessorApplication.java
@@ -0,0 +1,13 @@
+package com.moseoh.annotationprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotationprocessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnotationprocessorApplication.class, args);
+    }
+
+}
---
+++ spring/annotationprocessor/src/test/java/com/moseoh/annotationprocessor/AnnotationprocessorApplicationTests.java
@@ -0,0 +1,14 @@
+package com.moseoh.annotationprocessor;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnotationprocessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
